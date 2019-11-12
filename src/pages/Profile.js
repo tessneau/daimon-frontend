7e3451afce5ac9a1ae04e58163811133de530ed4
Tess Neau <tessneau@gmail.com>
2019-11-12 12:47:50 -0500

    Profile functional

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,19 +3,16 @@ import { connect } from 'react-redux';
 import PinnedPostsContainer from './PinnedPostsContainer';
 import '../style/Profile.scss';
 
-class Profile extends Component {
+const Profile = ({ username, avatar_img }) => {
 
-  render() {
     return (
       <div className="profile-container">
       <h1>FEED</h1>
-      <h3>Welcome {this.props.username}</h3>
-      <img src={this.props.avatar_img} height='240' width='240' alt="avatar"/>
+      <h3>Welcome {username}</h3>
+      <img src={avatar_img} height='240' width='240' alt="avatar"/>
       <PinnedPostsContainer />
       </div>
     );
-  }
-
 }
 
 const mapStateToProps = state => {
